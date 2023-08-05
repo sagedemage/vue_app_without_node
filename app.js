438cf61971081d2380bc83050511780cd217b980
Salmaan Saeed <salsaeed999@gmail.com>
2023-08-05 02:22:19 -0400

    added navbar component

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import { ref, computed } from 'vue'
 import Home from './components/home.js'
 import About from './components/about.js'
+import Navbar from './components/navbar.js'
 
 export default {
+	components: {
+		Navbar
+	},
 	setup() {
 		const count = ref(0)
 		const routes = {
@@ -21,24 +25,7 @@ export default {
 		return { count, currentView }
 	},
 	template: /*html*/`
-		<nav class="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-  			<div class="container-fluid">
-    			<a class="navbar-brand" href="#/">Vue W/o Node</a>
-    			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      				<span class="navbar-toggler-icon"></span>
-    			</button>
-    			<div class="collapse navbar-collapse" id="navbarNav">
-      				<ul class="navbar-nav">
-        				<li class="nav-item">
-          					<a class="nav-link" href="#/">Home</a>
-        				</li>
-        				<li class="nav-item">
-          					<a class="nav-link" href="#/about">About</a>
-        				</li>
-      				</ul>
-    			</div>
-  			</div>
-		</nav>
+		<Navbar />
 		<component :is="currentView" />
 	`
 }
